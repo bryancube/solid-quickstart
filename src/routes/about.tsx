68146ce4f2ae224b47dc704c15733903c80729fa
bryancube <87297322+bryancube@users.noreply.github.com>
2024-03-08 18:31:34 +0900

    Update about.tsx

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -4,6 +4,8 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2407917196575337"
+     crossorigin="anonymous"></script>
       <Title>About Us</Title>
       <h1>Clicker!</h1>
       This is everything! funny.
