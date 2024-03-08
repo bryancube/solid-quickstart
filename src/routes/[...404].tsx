09e1cf1a9f652ecf9ee49687695a2cb9e16b2495
bryancube <87297322+bryancube@users.noreply.github.com>
2024-03-08 18:31:26 +0900

    Update [...404].tsx

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -4,6 +4,8 @@ import { HttpStatusCode } from "solid-start/server";
 export default function NotFound() {
   return (
     <main>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2407917196575337"
+     crossorigin="anonymous"></script>
       <Title>Not Found</Title>
       <HttpStatusCode code={404} />
       <h1>Page Not Found</h1>
