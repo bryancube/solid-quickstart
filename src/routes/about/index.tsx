335b034996f0e86d884cb2ff10af9192bf8822a4
bryancube <87297322+bryancube@users.noreply.github.com>
2024-03-08 18:23:24 +0900

    Update index.tsx

--- src/routes/about/index.tsx
+++ src/routes/about/index.tsx
@@ -3,6 +3,10 @@ import Counter from "~/components/Counter";
 
 export default function Home() {
   return (
+    <head>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2407917196575337"
+     crossorigin="anonymous"></script>
+    </head>
     <main>
       <Title>About Us</Title>
       <h1>Clicker!</h1>
