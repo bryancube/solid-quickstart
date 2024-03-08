acf62573280312640d4f41230270aa6204a551aa
bryancube <87297322+bryancube@users.noreply.github.com>
2024-03-08 18:15:47 +0900

    Update index.tsx

--- src/routes/about/index.tsx
+++ src/routes/about/index.tsx
@@ -4,16 +4,11 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main>
-      <Title>About Us - Clicker</Title>
+      <Title>About Us</Title>
       <h1>Clicker!</h1>
-      This is something you can't think.
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
+      This is everything! funny.
+      abcdef
+      <a href=https://github.com/bryancube/solid-quickstart/tree/main>Github</a>
     </main>
   );
 }
